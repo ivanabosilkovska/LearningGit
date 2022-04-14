21fb9345c4097ad7fccc7b34bf35408b99da061e
Ivana <bosilkovska.ivana@gmail.com>
2022-04-14 20:57:56 +0200

    my first commit

---
+++ src/com/company/NewClass.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+
+    }
+
+    // ova e komentar
+    private int a = 5;
+/*
+    public int getA() {
+        return a;
+    }*/
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    private static void eat(){
+        System.out.println("Eating...");
+    }
+
+    public NewClass(int a) {
+        this.a = a;
+    }
+}
---
+++ src/validations/NewInterface.java
@@ -0,0 +1,4 @@
+package validations;
+
+public interface NewInterface {
+}

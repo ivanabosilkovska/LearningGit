0b12e7fc01a6d4c6682973d8b2a790b2a27ff9f7
Ivana <bosilkovska.ivana@gmail.com>
2022-04-18 21:16:34 +0200

    learning how to push

--- src/com/company/NewClass.java
+++ src/com/company/NewClass.java
@@ -8,6 +8,7 @@ public class NewClass {
         System.out.println("Learning to commit our code!");
         System.out.println("Amend command");
         System.out.println("Hello");
+        System.out.println("pushing changes");
 
     }
 
